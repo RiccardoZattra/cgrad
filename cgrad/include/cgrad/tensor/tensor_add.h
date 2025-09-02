ebb749aea1da5749c0eb6046b3ea704c8d77c489
RiccardoZattra <riccardo.zattra19@gmail.com>
2025-09-02 12:57:40 +0200

    Added doxygen documentation at files tensor_add.c and tensor_axpy.c. Furthermore, add operation between tensors now supported for 32/16 bit integers. New types addded on dtypes.h

--- cgrad/include/cgrad/tensor/tensor_add.h
+++ cgrad/include/cgrad/tensor/tensor_add.h
@@ -6,6 +6,24 @@
 #include "cgrad/autograd/computational_graph/computational_graph_link.h"
 #include "cgrad/cgrad_env.h"
 
+/**
+ * @brief Add to tensors anche update the computational graph
+ * 
+ * @param x Constant pointer to the first operand
+ * @param y Constant pointer to the second operand
+ * @param out Constant pointer to a pointer to store the result
+ * @param track_grad Flag selecting wheter to evaluate the gradient for the two operands
+ * @param env Pointer to a struct cgrad_env 
+ * 
+ * @return A value ::cgrad_error indicating the result of the operation
+ *         - TENSOR_NULL null pointer to x,y or out
+ *         - ALLOCATORS_NULL null pointer to allocators
+ *         - TENSOR_DATA_NULL no data insidere operands
+ *         - TENSOR_DATA_SIZE_MISMATCH data shape mismatch between the two operands
+ *         - TENSOR_SHAPE_MISMATCH shape mismatch between the two operands
+ *         - TENSOR_DTYPE_MISMATCH type mismatch between the two operands
+ *         - NO_ERROR no errors
+ */
 cgrad_error tensor_add(struct tensor *const x, struct tensor *const y, struct tensor **const out, const bool track_grad, struct cgrad_env *const env);
 
 #endif
--- cgrad/include/cgrad/tensor/tensor_axpy.h
+++ cgrad/include/cgrad/tensor/tensor_axpy.h
@@ -4,6 +4,18 @@
 #include "cgrad/tensor/tensor.h"
 #include "cgrad/error.h"
 
+/**
+ * @brief Perform a*x+y wiht x,y tensors of general type, 'a' scalar
+ * 
+ * @param x Constant pointer to the x
+ * @param y Constant pointer to the y 
+ * @param alpha Constant double for the scalar
+ * 
+ * @return A value ::cgrad_error indicating the result of the operation
+ *         - TENSOR_SHAPE_MISMATCH shape mismatch between the two operands 
+ *         - TENSOR_DTYPE_MISMATCH type mismatch between the two operands
+ *         - NO_ERROR no errors
+ */
 cgrad_error tensor_axpy(const struct tensor *const x, struct tensor *const y, const double a);
 
 #endif
